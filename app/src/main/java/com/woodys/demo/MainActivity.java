f4803c5f0f89976ae81f89f4db2a25fdd5504662
woody <yuetao.315@qq.com>
2016-10-08 16:53:20 +0800

    提交动态添加View的方式添加视频播放器

--- app/src/main/java/com/woodys/demo/MainActivity.java
+++ app/src/main/java/com/woodys/demo/MainActivity.java
@@ -3,10 +3,16 @@ package com.woodys.demo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.player.media.IjkVideoView;
 import com.player.player.IjkViderPlayer;
+import com.player.utils.PlayerManage;
+
+import tv.danmaku.ijk.media.player.IMediaPlayer;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +24,11 @@ public class MainActivity extends AppCompatActivity {
     private final String url2 = "http://weibo.com/p/23044451f0e5c4b762b9e1aa49c3091eea4d94";
     private IjkViderPlayer viderPlayer;
 
+    private IjkViderPlayer viderPlayer2;
+    public FrameLayout videoLayout;
+    public ImageView videoCover;
+    public ImageView videPlay;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +37,26 @@ public class MainActivity extends AppCompatActivity {
 
         viderPlayer = (IjkViderPlayer) findViewById(R.id.ijk_videoplayer);
 
+        videoLayout = (FrameLayout) findViewById(R.id.fl_video);
+        videoCover = (ImageView) findViewById(R.id.iv_video_cover);
+        videPlay = (ImageView) findViewById(R.id.iv_video_play);
+
         initPlayer();
+        initPlayer2();
+
+
+        videPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (viderPlayer2.isPlaying()) {
+                    return;
+                }
+                videoLayout.removeAllViews();
+                videoLayout.addView(viderPlayer2);
+                videoLayout.setVisibility(View.VISIBLE);
+                viderPlayer2.playVideo(url1, "视频地址");
+            }
+        });
 
     }
 
@@ -74,12 +104,57 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+    /**
+     * 初始化播放器
+     */
+    private void initPlayer2() {
+        viderPlayer2 = PlayerManage.getSuperManage().initialize(this);
+        // init player
+        //初始化相关参数(必须放在Play前面)
+        viderPlayer2.setIsNeedBatteryListen(true);
+        viderPlayer2.setIsNeedNetChangeListen(true);
+
+        //播放完成监听
+        viderPlayer2.setOnCompletionListener(new IjkViderPlayer.OnCompletionListener() {
+
+            @Override
+            public void onCompletion(IMediaPlayer mediaPlayer) {
+                ViewGroup view = (ViewGroup) viderPlayer2.getParent();//找到videoitemview的父类，然后remove
+                if (view != null && view.getChildCount() > 0) {
+                    view.removeAllViews();
+                    view.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        //网络监听
+        viderPlayer2.setOnNetChangeListener(new IjkViderPlayer.OnNetChangeListener() {
+            @Override
+            public void onWifi(IjkVideoView mediaPlayer) {
+            }
+
+            @Override
+            public void onMobile(IjkVideoView mediaPlayer) {
+                Toast.makeText(MainActivity.this, "请注意,当前网络状态切换为3G/4G网络", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNoAvailable(IjkVideoView mediaPlayer) {
+                Toast.makeText(MainActivity.this, "当前网络不可用,检查网络设置", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
     public void btn01(View view) {
         viderPlayer.playVideo(url1, "视频地址");
+        viderPlayer2.playVideo(url1, "视频地址");
     }
 
     public void btn02(View view) {
         viderPlayer.playVideo(url2, "错误连接！");
+        viderPlayer2.playVideo(url2, "错误连接！");
 
     }
 
@@ -97,6 +172,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         viderPlayer.pauseVideo();
+        viderPlayer2.pauseVideo();
     }
 
 
@@ -111,6 +187,10 @@ public class MainActivity extends AppCompatActivity {
         if(viderPlayer != null){
             viderPlayer.destroyVideo();
             viderPlayer = null;
+        }
+        if(viderPlayer2 != null){
+            viderPlayer2.destroyVideo();
+            viderPlayer2 = null;
         }
         super.onDestroy();
     }
---
+++ videoPlayerManager/build/generated/source/buildConfig/debug/com/player/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.player;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.player";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 330;
+  public static final String VERSION_NAME = "3.3.0";
+}
